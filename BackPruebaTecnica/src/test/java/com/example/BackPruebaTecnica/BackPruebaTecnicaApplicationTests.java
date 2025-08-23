656f16b9a104bdb6e8d1e9e7aef43bbf9407e04f
kevinLeonadoGamboa <kevingamboamosquera@gmail.com>
2025-08-22 22:02:49 -0500

    inicializando git

---
+++ BackPruebaTecnica/src/test/java/com/example/BackPruebaTecnica/BackPruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackPruebaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
